529f832b1a90fd5aa2bc2516b9b3e51973919954
jessicathaisaalmeida <71471047+jessicathaisaalmeida@users.noreply.github.com>
2020-10-09 20:17:30 -0300

    Plotting chart data.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,19 @@ export default class App extends React.Component {
     this.name = "Jéssica Thaisa Almeida";
 
     this.state = {
-      data: `{type: 'start', timestamp: 1519780251293, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']}
-{type: 'span', timestamp: 1519780251293, begin: 1519780251293, end: 1519780260201}
-{type: 'data', timestamp: 1519780251000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
-{type: 'stop', timestamp: 1519780251293}`,
+      data: `{type: 'start', timestamp: 1604926800000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']}
+{type: 'span', timestamp: 1604890800000, begin: 1519780251293, end: 1606014000000}
+{type: 'data', timestamp: 1604926800000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.5}
+{type: 'data', timestamp: 1604926800000, os: 'windows', browser: 'firefox', min_response_time: 0.0, max_response_time: 2.3}
+{type: 'data', timestamp: 1604926800000, os: 'fedora', browser: 'firefox', min_response_time: 0.1, max_response_time: 2.1}
+{type: 'data', timestamp: 1604926800000, os: 'mac os', browser: 'safari', min_response_time: 0.3, max_response_time: 1.3}
+{type: 'data', timestamp: 1605409200000, os: 'windows', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
+{type: 'data', timestamp: 1605409200000, os: 'windows', browser: 'firefox', min_response_time: 0.5, max_response_time: 1.8}
+{type: 'data', timestamp: 1605409200000, os: 'fedora', browser: 'firefox', min_response_time: 0.6, max_response_time: 2.9}
+{type: 'data', timestamp: 1605409200000, os: 'fedora', browser: 'chrome', min_response_time: 0.15, max_response_time: 1.3}
+{type: 'data', timestamp: 1605409200000, os: 'mac os', browser: 'safari', min_response_time: 0.45, max_response_time: 2.7}
+{type: 'data', timestamp: 1606100400000, os: 'linux', browser: 'chrome', min_response_time: 0.3, max_response_time: 2.8}
+{type: 'stop', timestamp: 1604926800000}`,
       currentData: ""
     };
   }
--- src/chartPlotting/ChartArea.js
+++ src/chartPlotting/ChartArea.js
@@ -29,7 +29,7 @@ function getArrayFromString(data) {
 function getPropsFromSimpleString(data) {
   let props = {};
   data.split(",").forEach((prop) => {
-    let propName = prop.split(":")[0];
+    let propName = prop.split(":")[0].trim();
     let propValue = prop.split(":")[1];
     propValue = propValue.trim();
     propValue = getValueFromString(propValue);
@@ -64,7 +64,7 @@ function getDataInstructions(informedData) {
             if (propsDefinitions[key] === "Array") {
               final = row.indexOf("]");
               let propsAuxiliar = row.substring(0, final);
-              let propName = propsAuxiliar.split(":")[0];
+              let propName = propsAuxiliar.split(":")[0].trim();
               let propValue = propsAuxiliar.split(":")[1];
               propValue = getArrayFromString(propValue);
               props[propName] = propValue;
@@ -95,38 +95,101 @@ function getDataInstructions(informedData) {
 
   return instructions;
 }
+
+function isInPast(moment) {
+  if (moment < new Date().getTime()) return true;
+  return false;
+}
+
+const randomColor = ((opacity) => {
+  const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  return (opacity) => {
+    var r = randomInt(0, 255);
+    var g = randomInt(0, 255);
+    var b = randomInt(0, 255);
+
+    return `rgba(${r},${g},${b},${opacity})`;
+  };
+})();
+
 function prepareDataChart(informedData) {
   if (!informedData) return null;
 
   let instructions = getDataInstructions(informedData);
 
-  console.log(instructions);
+  let chartLabels = [];
+  let chartGroups = [];
+  let datasets = {};
+  let plotDataset = [];
+  instructions.forEach((instruction) => {
+    if (instruction.type === "start") {
+      chartLabels = instruction.select;
+      chartGroups = instruction.group;
+    } else if (instruction.type === "data") {
+      if (isInPast(instruction.timestamp)) return;
+      let dataSetKey = "";
+      let chartLabel = "";
+      chartGroups.forEach((group) => {
+        dataSetKey += instruction[group] + "_#_";
+        chartLabel += instruction[group] + " ";
+      });
+      if (!datasets[dataSetKey]) {
+        datasets[dataSetKey] = {};
+      }
+      chartLabels.forEach((label) => {
+        //if it doesn't exists yet, here I create the basis structure
+        if (!datasets[dataSetKey][label]) {
+          let color = randomColor(0.6);
+          let dataset = {
+            label: (chartLabel + label.replaceAll("_", " "))
+              .split(/ /g)
+              .map(
+                (word) =>
+                  `${word.substring(0, 1).toUpperCase()}${word.substring(1)}`
+              )
+              .join(" "),
+            type: "line",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: color,
+            borderColor: color,
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: color,
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: color,
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 7,
+            pointHitRadius: 10,
+            data: []
+          };
+          datasets[dataSetKey][label] = dataset;
+        }
+        datasets[dataSetKey][label]["data"].push({
+          x: instruction["timestamp"],
+          y: instruction[label]
+        });
+      });
+    } else if (instruction.type === "stop") {
+      return;
+    }
+  });
+
+  Object.keys(datasets).forEach((group) => {
+    Object.keys(datasets[group]).forEach((label) => {
+      plotDataset.push(datasets[group][label]);
+    });
+  });
 
   return {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: "rgba(75,192,192,0.4)",
-        borderColor: "rgba(75,192,192,1)",
-        borderCapStyle: "butt",
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: "miter",
-        pointBorderColor: "rgba(75,192,192,1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
+    datasets: plotDataset
   };
 }
 
@@ -134,7 +197,6 @@ export default class ChartArea extends React.Component {
   render() {
     return (
       <div className="ChartArea">
-        <div>{this.props.data.split("\n")[1]}</div>
         <Line data={prepareDataChart(this.props.data)} />
         <div style={GenerateChartArea}>
           <Button
