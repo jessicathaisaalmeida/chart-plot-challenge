61c83d6888da68cd4fed749ff4ecf111f472c136
jessicathaisaalmeida <71471047+jessicathaisaalmeida@users.noreply.github.com>
2020-10-09 22:06:34 -0300

    Generating chart with labels

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ export default class App extends React.Component {
     this.name = "Jéssica Thaisa Almeida";
 
     this.state = {
-      data: `{type: 'start', timestamp: 1604926800000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']}
-{type: 'span', timestamp: 1604890800000, begin: 1519780251293, end: 1606014000000}
-{type: 'data', timestamp: 1604926800000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.5}
-{type: 'data', timestamp: 1604926800000, os: 'windows', browser: 'firefox', min_response_time: 0.0, max_response_time: 2.3}
-{type: 'data', timestamp: 1604926800000, os: 'fedora', browser: 'firefox', min_response_time: 0.1, max_response_time: 2.1}
-{type: 'data', timestamp: 1604926800000, os: 'mac os', browser: 'safari', min_response_time: 0.3, max_response_time: 1.3}
-{type: 'data', timestamp: 1605409200000, os: 'windows', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
-{type: 'data', timestamp: 1605409200000, os: 'windows', browser: 'firefox', min_response_time: 0.5, max_response_time: 1.8}
-{type: 'data', timestamp: 1605409200000, os: 'fedora', browser: 'firefox', min_response_time: 0.6, max_response_time: 2.9}
-{type: 'data', timestamp: 1605409200000, os: 'fedora', browser: 'chrome', min_response_time: 0.15, max_response_time: 1.3}
-{type: 'data', timestamp: 1605409200000, os: 'mac os', browser: 'safari', min_response_time: 0.45, max_response_time: 2.7}
-{type: 'data', timestamp: 1606100400000, os: 'linux', browser: 'chrome', min_response_time: 0.3, max_response_time: 2.8}
-{type: 'stop', timestamp: 1604926800000}`,
+      data: `{type: 'start', timestamp: 1605013270000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']}
+{type: 'span', timestamp: 1605013270000, begin: 1605013270000, end: 1605013330000}
+{type: 'data', timestamp: 1605013280000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.5}
+{type: 'data', timestamp: 1605013290000, os: 'windows', browser: 'firefox', min_response_time: 0.0, max_response_time: 2.3}
+{type: 'data', timestamp: 1605013310000, os: 'fedora', browser: 'firefox', min_response_time: 0.1, max_response_time: 2.1}
+{type: 'data', timestamp: 1605013320000, os: 'mac os', browser: 'safari', min_response_time: 0.3, max_response_time: 1.3}
+{type: 'data', timestamp: 1605013320000, os: 'windows', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
+{type: 'data', timestamp: 1605013330000, os: 'windows', browser: 'firefox', min_response_time: 0.5, max_response_time: 1.8}
+{type: 'data', timestamp: 1605013330000, os: 'fedora', browser: 'firefox', min_response_time: 0.6, max_response_time: 2.9}
+{type: 'data', timestamp: 1605013330000, os: 'fedora', browser: 'chrome', min_response_time: 0.15, max_response_time: 1.3}
+{type: 'data', timestamp: 1605013330000, os: 'mac os', browser: 'safari', min_response_time: 0.45, max_response_time: 2.7}
+{type: 'data', timestamp: 1605013360000, os: 'linux', browser: 'chrome', min_response_time: 0.3, max_response_time: 2.8}
+{type: 'stop', timestamp: 1605013360000}`,
       currentData: ""
     };
   }
