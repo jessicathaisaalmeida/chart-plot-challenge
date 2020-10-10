89556bc062ff653b64584d7de96266e9d2c40753
jessicathaisaalmeida <71471047+jessicathaisaalmeida@users.noreply.github.com>
2020-10-10 13:27:35 -0300

    refactoring of event stream parser

--- src/components/__tests__/eventStreamParser.test.js
+++ src/components/__tests__/eventStreamParser.test.js
@@ -30,6 +30,17 @@ it("should parse a single quotted array to a string", () => {
   expect(parser.getValueFromString(string)).toBe("['a','b']");
 });
 
+it("should parse an array to a array", () => {
+  let string = "['a','b']";
+  expect(parser.getValueFromString(string)).toStrictEqual(["a", "b"]);
+});
+
+//Test ignored to facilitate
+/*it("should parse an array of single quotted arrays to a array of strings", () => {
+  let string = "['['a']','['b','c']']";
+  expect(parser.getValueFromString(string)).toBe(["['a']", "['b','c']"]);
+});*/
+
 it("should return null when data is null", () => {
   let string = null;
   expect(parser.getValueFromString(string)).toBe(null);
@@ -38,4 +49,58 @@ it("should return null when data is null", () => {
 it("should return null when data string is null", () => {
   let string = "null";
   expect(parser.getValueFromString(string)).toBe(null);
+});
+
+it("should return a string prop", () => {
+  let string = "type : 'start'";
+  expect(parser.getPropsFromSimpleString(string)).toStrictEqual({
+    type: "start"
+  });
+});
+
+it("should return a number prop", () => {
+  let string = "value : 1234";
+  expect(parser.getPropsFromSimpleString(string)).toStrictEqual({
+    value: 1234
+  });
+});
+
+it("should get an object with simple props", () => {
+  let string = "type: 'stop', timestamp: 1605013270000";
+  expect(parser.getPropsFromSimpleString(string)).toStrictEqual({
+    type: "stop",
+    timestamp: 1605013270000
+  });
+});
+
+it("should find the string Patch for a string key", () => {
+  let string =
+    "type: 'start', timestamp: 1605013270000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']";
+  expect(parser.findPropStringInStringEvent("type", "", string)).toBe(
+    "type: 'start'"
+  );
+});
+
+it("should find the string Patch for a number key", () => {
+  let string =
+    "type: 'start', timestamp: 1605013270000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']";
+  expect(parser.findPropStringInStringEvent("timestamp", "", string)).toBe(
+    "timestamp: 1605013270000"
+  );
+});
+
+it("should find the string Patch for a array key", () => {
+  let string =
+    "type: 'start', timestamp: 1605013270000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']";
+  expect(parser.findPropStringInStringEvent("select", "Array", string)).toBe(
+    "select: ['min_response_time', 'max_response_time'"
+  );
+});
+
+it("shouldn't find the string Patch for an absent key", () => {
+  let string =
+    "type: 'start', timestamp: 1605013270000, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']";
+  expect(parser.findPropStringInStringEvent("test", "Array", string)).toBe(
+    null // TODO: throw an exception
+  );
 });
--- src/components/eventStreamParser.js
+++ src/components/eventStreamParser.js
@@ -11,78 +11,106 @@ function getArrayFromString(data) {
   if (!data) return null;
   data = data.trim().substring(1, data.length - 1);
   let theArray = [];
+
   data.split(",").forEach((element) => {
     theArray.push(getValueFromString(element));
   });
   return theArray;
 }
 
-function getPropsFromSimpleString(data) {
+export const getPropsFromSimpleString = (data) => {
   let props = {};
+  if (!data) return null;
   data.split(",").forEach((prop) => {
     let propName = prop.split(":")[0].trim();
     let propValue = prop.split(":")[1];
-    propValue = propValue.trim();
-    propValue = getValueFromString(propValue);
+    if (propValue) {
+      propValue = propValue.trim();
+      propValue = getValueFromString(propValue);
+    }
 
     props[propName] = propValue;
   });
   return props;
-}
+};
 
-const propsDefinitions = {
-  timestamp: "Number",
-  select: "Array",
-  group: "Array"
+export const findPropStringInStringEvent = (key, theType, eventString) => {
+  if (eventString.indexOf(key) === -1) return null;
+  if (theType === "Array") {
+    let final = eventString.indexOf("]", eventString.indexOf(key));
+
+    //separate the key from the value of the property and treat the value
+    return eventString.substring(eventString.indexOf(key), final);
+  } else {
+    let final = eventString.indexOf(",", eventString.indexOf(key));
+    return eventString.substring(eventString.indexOf(key), final);
+  }
+};
+
+export const parseEventStringToObject = (eventString) => {
+  /* a string is very well formed:
+   *  with the keys:
+   *      type: "String",
+   *      timestamp: "Number",
+   *      select: "Array",
+   *      group: "Array"
+   *  each kind of prop, from the definition, is searched in the eventString
+   */
+  let startPropsDefinitions = {
+    type: "String",
+    timestamp: "Number",
+    select: "Array",
+    group: "Array"
+  };
+
+  let props = {};
+
+  //if type is start we get O
+
+  eventString = eventString.replace("{", "").replace("}", "");
+
+  let isStart =
+    getPropsFromSimpleString(
+      findPropStringInStringEvent("type", "String", eventString)
+    ).type === "start";
+
+  if (isStart)
+    Object.keys(startPropsDefinitions).forEach((key) => {
+      if (eventString.indexOf(key) !== -1) {
+        let eventPropString = findPropStringInStringEvent(
+          key,
+          startPropsDefinitions[key],
+          eventString
+        );
+        if (startPropsDefinitions[key] === "Array") {
+          //separate the key from the value of the property and treat the value
+          let propName = eventPropString.split(":")[0].trim();
+          let propValue = eventPropString.split(":")[1];
+          propValue = getArrayFromString(propValue);
+
+          //generate the key/value pair of the prop
+          props[propName] = propValue;
+        } else {
+          //get the prop (key/value pair) from the patch
+          let propsAuxiliar = getPropsFromSimpleString(
+            eventPropString.substring(eventPropString.indexOf("{") + 1)
+          );
+          props[key] = propsAuxiliar[key];
+        }
+      }
+    });
+  else return getPropsFromSimpleString(eventString);
+  return props;
 };
 
 export const parseEventStreamToList = (eventStream) => {
-  let instructions = [];
-  eventStream.split("\n").forEach((row) => {
-    row = row
-      .trim()
-      .substring(1, row.length - 1)
-      .split("type:")[1]
-      .trim();
-
-    if (row.indexOf("start") !== -1) {
-      while (row.indexOf(",") !== -1) {
-        row = row.substring(row.indexOf(",") + 1).trim();
-        let props = {};
-        Object.keys(propsDefinitions).forEach((key) => {
-          if (row.indexOf(key) !== -1) {
-            let final = 0;
-            if (propsDefinitions[key] === "Array") {
-              final = row.indexOf("]");
-              let propsAuxiliar = row.substring(0, final);
-              let propName = propsAuxiliar.split(":")[0].trim();
-              let propValue = propsAuxiliar.split(":")[1];
-              propValue = getArrayFromString(propValue);
-              props[propName] = propValue;
-              row = row.substring(final + 1);
-              final = row.indexOf(",");
-            } else {
-              final = row.indexOf(",");
-              let propsAuxiliar = getPropsFromSimpleString(
-                row.substring(0, final)
-              );
-              props[key] = propsAuxiliar[key];
-            }
-            row = row.substring(final + 1);
-          }
-        });
-
-        if (Object.keys(props).length > 0) {
-          props["type"] = "start";
-          instructions.push(props);
-        }
-      }
-    } else {
-      let props = getPropsFromSimpleString(row.substring(row.indexOf(",") + 1));
-      props["type"] = row.substring(1, row.indexOf(",") - 1).trim();
-      instructions.push(props);
-    }
+  let eventList = [];
+  eventStream.split("\n").forEach((eventString) => {
+    // spcilly treats a start type event because of the arrays inside
+    let event = parseEventStringToObject(eventString);
+    if (event && Object.keys(event).length > 0) eventList.push(event);
   });
+  console.log(eventList);
 
-  return instructions;
+  return eventList;
 };
